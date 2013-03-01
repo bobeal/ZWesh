7e55586294ec0560a271e43de31d16ad84d99aba
ybr <ybr@zenexity.com>
2013-03-01 14:53:24 +0100

    [CapDemat] render instant view

--- app/controllers/Capdemat.scala
+++ app/controllers/Capdemat.scala
@@ -7,4 +7,24 @@ object Capdemat extends Controller {
 
   def index = TODO
 
+  def dashboard = Action {
+    import play.api.libs.json._
+    Ok(views.html.widgets.capdemat.capdematList(Json.obj(
+        "widget-title" -> "CapDemat",
+        "cities" -> JsArray(Seq(
+            Json.obj("name" -> "Pessac",
+                    "userCount" -> 8948,
+                    "askCount" -> 30929,
+                    "paymentCount" -> 3727),
+            Json.obj("name" -> "Beaucouzé",
+                    "userCount" -> 609,
+                    "askCount" -> 2861,
+                    "paymentCount" -> 2156),
+            Json.obj("name" -> "Roubaix",
+                    "userCount" -> 10649,
+                    "askCount" -> 31196,
+                    "paymentCount" -> 0)
+        ))
+    )))
+  }
 }
