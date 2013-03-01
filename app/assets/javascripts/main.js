9c8e8e0e612772c308f5d5a46339fb6e412d3c78
ybr <ybr@zenexity.com>
2013-03-01 15:49:11 +0100

    Widget abstraction and registration

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,4 +14,44 @@ require(['jquery', 'gridster'], function($) {
   , widget_base_dimensions: [140, 140]
   })
 
+    // load widgets
+    var gridster = $(".gridster ul").gridster().data("gridster");
+    var widgets = [];
+
+    // load capdemat widget
+    var capdematWidget = (function() {
+      var widget = null;
+      return {
+        init: function() {
+          $.ajax("/capdemat/dashboard").then(function(html) {
+            widget = gridster.add_widget("<li></li>", 3, 4);
+            widget.html(html);
+          });
+        },
+        update: function() {
+          $.ajax("/capdemat/dashboard").then(function(html) {
+            widget.html(html);
+          });
+        }
+      }
+    })();
+
+    // register widgets
+    widgets.push(capdematWidget);
+
+    function initWidgets() {
+      widgets.forEach(function(widget) { widget.init(); });
+    }
+
+    function updateWidgets() {
+      widgets.forEach(function(widget) { widget.update(); });
+    }
+
+    function widgetDaemon() {
+      updateWidgets();
+      setTimeout(widgetDaemon, 2000);
+    }
+
+    initWidgets();
+    widgetDaemon();
 })
